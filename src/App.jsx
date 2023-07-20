3de7f9a2e4dcda923d86c148eec05b7dbd0673d0
MujuniEli <elijahmujuni@gmail.com>
2023-07-20 07:01:31 +0300

    Added mark-up to app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import Nav from './components/Nav'
 import './App.css'
+import TrackingCodeSelector from './components/TaskCodeSelector'
 
 function App() {
   
@@ -8,6 +9,8 @@ function App() {
   return (
             <div className="app">
                 <Nav />
+                <TrackingCodeSelector />
+            
             </div>
   )
 }
