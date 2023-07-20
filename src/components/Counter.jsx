14820d6e7539a1ba17fbcb6ff5ae6630f8e41ae0
MujuniEli <elijahmujuni@gmail.com>
2023-07-20 07:46:16 +0300

    Added increment function

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -13,4 +13,34 @@ export default function Counter() {
 
     //function to format numbers with leading zeros if needed
     const formatNumber = (num) => (num < 0 ? `0${num}` : num);
+
+    //function to increment the counter by one second
+    const incrementCounter = () => {
+        setSeconds((prevSeconds) => {
+            if (prevSeconds === 59) {
+                //if seconds reach 59, reset to zero and increment minutes
+                setMinutes((prevMinutes) => {
+                    if (prevMinutes === 59) {
+                        //if minutes reach 59, reset to zero and increment hours
+                        setHours((prevHours) => {
+                            if (prevHours === 23) {
+                                //if hours reach 23, reset to zero and increment days
+                                setDays((prevDays) => prevDays + 1);
+                                return 0;
+                            }else {
+                                return prevHours + 1;
+                            }
+                        });
+                        return 0;
+                    }else {
+                        return prevMinutes + 1;
+                    }
+                });
+                return 0;
+            } else {
+                return prevSeconds + 1;
+            }
+        });
+        
+    };
 }
