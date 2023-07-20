7527e591aa9f399635546ddc816f51245d3f1cea
MujuniEli <elijahmujuni@gmail.com>
2023-07-20 06:55:36 +0300

    Added task code drop down selector

---
+++ src/components/TaskCodeSelector.jsx
@@ -0,0 +1,15 @@
+
+export default function TrackingCodeSelector () {
+
+    return (
+                <form action="">
+                    <label htmlFor="">Select a task code from the list to track time spent working with that code: </label>
+                    <select name="" id="">
+                        <option value="task code one">task code one</option>
+                        <option value="task code two">task code two</option>
+                        <option value="task code three">task code three</option>
+                        <option value="task code four">task code four</option>
+                    </select>
+                </form>
+    )
+}
