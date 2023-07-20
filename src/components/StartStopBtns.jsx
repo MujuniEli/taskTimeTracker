8d04b72696877de844d57e270fe5f1b52813ab1f
MujuniEli <elijahmujuni@gmail.com>
2023-07-20 06:53:15 +0300

    Added start & stop button mark up

---
+++ src/components/StartStopBtns.jsx
@@ -0,0 +1,10 @@
+
+export default function StartStopBtns () {
+
+    return (
+                <div>
+                    <button>Start</button>
+                    <button>Stop</button>
+                </div>
+    )
+}
