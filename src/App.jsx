d0c8c62d421b90ba27ed004461e3c07196c945a4
MujuniEli <elijahmujuni@gmail.com>
2023-07-22 12:44:10 +0300

    Added Admin dashboard, signIn & signUp Routes to App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 
+import { Routes, Route } from 'react-router-dom'
+
+import Admin from './pages/Admin'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 import Nav from './components/Nav'
 import TrackingCodeSelector from './components/TaskCodeSelector'
 import Counter from './components/Counter'
@@ -11,6 +16,11 @@ function App() {
   return (
             <div className="app">
                 <Nav />
+                <Routes>
+                    <Route path='' element={<Admin/>} />
+                    <Route path='' element={<SignIn />} />
+                    <Route path='' element={<SignUp />} />
+                </Routes>
                 <TrackingCodeSelector />
                 <Counter />
                 <History />
