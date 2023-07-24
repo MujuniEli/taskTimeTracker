7a56f28d0900dfe7aa2c7282ea24b80d855cfb65
MujuniEli <elijahmujuni@gmail.com>
2023-07-24 08:47:09 +0300

    Added sign in form mark up

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,8 +1,22 @@
+import { useFormik } from 'formik'
+
 export default function SignIn () {
+    const formik = useFormik({
+        initialValues: {
+            email:"",
+        },
+    })
     
     return (
                 <div className="admin">
                     <h3>This is the sign in panel</h3>
+                    <label htmlFor="email"></label>
+                    <input
+                        value={formik.values.email}
+                        id='email' 
+                        type='email'
+                        placeholder='Email' 
+                    />
                 </div>
     )
 }
