391e02d8585c4e1d71c3ccc5696c7305f6daa0af
MujuniEli <elijahmujuni@gmail.com>
2023-07-25 07:30:26 +0300

    Schema updates

--- src/Schemas/index.jsx
+++ src/Schemas/index.jsx
@@ -1,6 +1,9 @@
 import * as yup from "yup";
 
+const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/;
+
 export const myFormSchema = yup.object().shape({
     email: yup.string().email("Please enter a valid email").required("required"),
-    password: yup.string().min(5).matches(passwordRules, {"Please create a stronger password"}).required("required")
-})
+    password: yup.string().min(5).matches(passwordRules, {message: "Please create a stronger password" }).required("required"),
+    confirmPassword: yup.string().oneOf([yup.ref("password"), null], "Passwords must match").required("required"),
+});
