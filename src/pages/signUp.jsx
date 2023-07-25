0544cece5ab9bb7dd0c96ce04aa9ed7853021061
MujuniEli <elijahmujuni@gmail.com>
2023-07-25 06:53:58 +0300

    Added sign up from mark up

--- src/pages/signUp.jsx
+++ src/pages/signUp.jsx
@@ -1,8 +1,29 @@
+import { useFormik } from "formik"
+
 export default function SignUp () {
     
     return (
                 <div className="signUp">
                     <h3>This is the sign up panel</h3>
+
+                    <form action="">
+                        <input
+                        id="email" 
+                        type="email" 
+                        placeholder="email"
+                        />
+                        <input 
+                        id="password"
+                        type="password"
+                        placeholder="Create password" 
+                        />
+
+                        <input
+                        id="confirmPassword" 
+                        type="password" 
+                        placeholder="Confirm Password"
+                        />
+                    </form>
                 </div>
     )
 }
